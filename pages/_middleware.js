fc8132b810f7dfd63cff454039b4d106f8604c29
Akash Sachan <akashsachan@Akashs-MacBook-Air.local>
2022-05-27 18:00:13 +0530

    fixed reroute

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -2,6 +2,9 @@ import { getToken } from "next-auth/jwt"
 import { NextResponse } from "next/server"
 
 export async function middleware(req) {
+    const url = req.nextUrl.clone()
+    url.pathname = '/login'
+
     //token will exist if user is logged in
     const token = await getToken({ req, secret: process.env.JWT_SECRET })
 
@@ -11,13 +14,13 @@ export async function middleware(req) {
     // 1) Its a request for next-auth session and provider fetching
     // 2) the token exists
 
-    if(pathname.includes('/api/auth') || token){
+    if (pathname.includes('/api/auth') || token) {
         return NextResponse.next();
     }
 
     // Redirect to login if no token and requesting to protected route
 
-    if(!token && pathname !== "/login"){
-        return NextResponse.redirect(`http://localhost:3001/login`)  
-    }
+    if (!token && pathname !== url.pathname) {
+        return NextResponse.rewrite(url)
+      }
 }
